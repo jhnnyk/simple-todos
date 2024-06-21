7e014bd38697be810e26706fbe15c64efae15fd9
John Knotts <jhnnyk@gmail.com>
2024-06-21 13:52:19 -0600

    signup UI

--- app/register/page.js
+++ app/register/page.js
@@ -1,33 +1,44 @@
+import Link from 'next/link'
+import { FcGoogle } from 'react-icons/fc'
+
 const Register = () => {
   return (
-    <main>
-      <div>
-        <div>
-          <h1>Sign Up</h1>
-          <p>
-            Already have an account?
+    <main className="flex lg:h-[100vh]">
+      <div className="w-full lg:w-[60%] p-8 md:p-14 flex items-center justify-center lg:justify-start">
+        <div className="p-8 w-[600px]">
+          <h1 className="text-6xl font-semibold">Sign Up</h1>
+          <p className="mt-6 ml-1">
+            Already have an account?<br />
+            <Link href={'/login'} className="underline hover:text-blue-400 cursor-pointer">
+              Login
+            </Link>
           </p>
-          <div>
-            <span>
+          <div className='bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active: scale-80 flex justify-center items-center gap-4 cursor-pointer'>
+            <FcGoogle size={22} />
+            <span className='font-medium text-black group-hover:text-white'>
               Login with Google
             </span>
           </div>
           <form>
-            <div>
+            <div className='mt-10 pl-1 flex flex-col'>
               <label>Name</label>
-              <input type="text" required />
+              <input type="text" className='font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400' required />
             </div>
-            <div>
+            <div className='mt-10 pl-1 flex flex-col'>
               <label>Email</label>
-              <input type="email" required />
+              <input type="email" className='font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400' required />
             </div>
-            <div>
+            <div className='mt-10 pl-1 flex flex-col'>
               <label>Password</label>
-              <input type="password" required />
+              <input type="password" className='font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400' required />
             </div>
-            <button>Sign Up</button>
+            <button className='bg-black text-white w-44 py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90'>Sign Up</button>
           </form>
         </div>
+      </div>
+
+      <div className='w-[40%] bg-slate-400 bg-cover bg-right-top hidden lg:block' style={{backgroundImage: "url('/login.jpg')"}}>
+
       </div>
     </main>
   )
