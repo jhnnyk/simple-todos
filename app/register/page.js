27f451e50d606b59faacb9226962c7a61ec7fb36
John Knotts <jhnnyk@gmail.com>
2024-06-22 06:22:26 -0600

    sign up with Google

--- app/register/page.js
+++ app/register/page.js
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import { useState } from 'react'
 import { FcGoogle } from 'react-icons/fc'
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { createUserWithEmailAndPassword, updateProfile, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { auth } from '../../firebase/config'
 
 const Register = () => {
@@ -10,6 +10,7 @@ const Register = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  // sign up with email and password
   const handleSubmit = async (e) => {
     e.preventDefault()
     if (!name || !email || !password) return
@@ -20,6 +21,18 @@ const Register = () => {
       console.log(user)
     } catch (error) {
       console.error(error)
+    }
+  }
+
+  // sign up with Google
+  const provider = new GoogleAuthProvider()
+
+  const signUpWithGoogle = async () => {
+    try {
+      const user = await signInWithPopup(auth, provider)
+      console.log(user)
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -36,45 +49,49 @@ const Register = () => {
             </Link>
           </p>
 
-          <div className='bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-80 flex justify-center items-center gap-4 cursor-pointer'>
+          {/* sign up with Google button */}
+          <div
+            onClick={signUpWithGoogle}
+            className='bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-80 flex justify-center items-center gap-4 cursor-pointer'>
             <FcGoogle size={22} />
             <span className='font-medium text-black group-hover:text-white'>
-              Login with Google
+              Sign up with Google
             </span>
           </div>
 
+          {/* sign up with email and password form */}
           <form onSubmit={handleSubmit}>
             <div className='mt-10 pl-1 flex flex-col'>
               <label>Name</label>
-              <input 
-                type="text" 
-                className='font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400' 
-                required 
+              <input
+                type="text"
+                className='font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400'
+                required
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
 
             <div className='mt-10 pl-1 flex flex-col'>
               <label>Email</label>
-              <input 
-                type="email" 
-                className='font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400' 
-                required 
+              <input
+                type="email"
+                className='font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400'
+                required
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
             <div className='mt-10 pl-1 flex flex-col'>
               <label>Password</label>
-              <input 
-                type="password" 
-                className='font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400' 
-                required 
+              <input
+                type="password"
+                className='font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400'
+                required
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
-            <button 
+            <button
               className='bg-black text-white w-44 py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90'>
               Sign Up
             </button>
@@ -82,7 +99,7 @@ const Register = () => {
         </div>
       </div>
 
-      <div className='w-[40%] bg-slate-400 bg-cover bg-right-top hidden lg:block' style={{backgroundImage: "url('/login.jpg')"}}>
+      <div className='w-[40%] bg-slate-400 bg-cover bg-right-top hidden lg:block' style={{ backgroundImage: "url('/login.jpg')" }}>
 
       </div>
     </main>
